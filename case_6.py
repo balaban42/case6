fe980a0a57cad46a6f47eb090234b9ebd104dcbc
balaban42 <thebalmiha42@gmail.com>
2018-12-01 16:39:26 +0700

    Second commit case.

--- case_6.py
+++ case_6.py
@@ -20,3 +20,20 @@ def get_num_hexagons():
     except ValueError:
         print(local.ERROR2, end="")
         return get_num_hexagons()
+
+
+def get_color_choice():
+    """Filling color"""
+    colors_lst = [local.RED, local.ORANGE, local.YELLOW, local.GREEN, local.BLUE1, local.BLUE2, local.PURPLE]
+    codes = ['#FF0000', '#FFA500', '#FFFF00', '#00FF00', '#87CEEB', '#0000FF', '#800080']
+    color = str(input(local.IN_COLOR)).strip()
+    color = color.lower()
+    error_string = "'" + color + "' " + local.ERROR
+    for i in range(7):
+        if color == colors_lst[i]:
+            return codes[i]
+    while True:
+        color = str(input(error_string))
+        for i in range(7):
+            if color == colors_lst[i]:
+                return codes[i]
